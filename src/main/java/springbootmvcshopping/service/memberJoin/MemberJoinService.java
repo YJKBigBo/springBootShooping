2fcca006f38abe3dd3e693e62a50e0b437c82c0c
YJKBigBo <yjg0557@naver.com>
2024-10-22 16:39:07 +0900

    refactor : SMS 보내기

--- src/main/java/springbootmvcshopping/service/memberJoin/MemberJoinService.java
+++ src/main/java/springbootmvcshopping/service/memberJoin/MemberJoinService.java
@@ -38,16 +38,16 @@ public class MemberJoinService {
         //메일링 코드
         //1.try catch문으로 에러가 생기지 않았을때 메일링 실행
         //2.CUD는 정수를 반환받기 때문에 int를 반환받아 갯수가 0개 이상일때 만 실행되는 로직
-        if(i>0){
-            String content = "<html><body>";
-                    content += memberDTO.getMemberEmail() + "님 가입을 환영합니다.";
-                    content += "가입을 완료하시려면";
-                    content += "<a href='http://localhost:8080/userConfirm?chk="+memberDTO.getMemberEmail()+"'>여기</a>를 클릭하세요.";
-                    content += "</html></body>";
-            String subject = "메일 테스트";
-            String fromEmail = "highland0@nate.com";
-            String toEmail = memberDTO.getMemberEmail();
-            emailSendService.mailSend(fromEmail, toEmail, subject, content);
-        }
+//        if(i>0){
+//            String content = "<html><body>";
+//                    content += memberDTO.getMemberEmail() + "님 가입을 환영합니다.";
+//                    content += "가입을 완료하시려면";
+//                    content += "<a href='http://localhost:8080/userConfirm?chk="+memberDTO.getMemberEmail()+"'>여기</a>를 클릭하세요.";
+//                    content += "</html></body>";
+//            String subject = "메일 테스트";
+//            String fromEmail = "highland0@nate.com";
+//            String toEmail = memberDTO.getMemberEmail();
+//            emailSendService.mailSend(fromEmail, toEmail, subject, content);
+//        }
     }
 }
